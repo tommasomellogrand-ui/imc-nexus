c51091a6310fd20c3447267a5e7b219dd2b14cf7
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-23 14:34:27 +0200

    Load GW001 match report beta module

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -6,6 +6,7 @@
   if(document.readyState==="loading"){
     writeScript("js/app-core-53.js?v=53.0-core");
     writeScript("js/imc-results-scorers.js?v=1.0.0");
+    writeScript("js/imc-match-report-beta.js?v=1.0.0");
     return;
   }
   var core=document.createElement("script");
@@ -13,6 +14,11 @@
   core.onload=function(){
     var scorers=document.createElement("script");
     scorers.src="js/imc-results-scorers.js?v=1.0.0";
+    scorers.onload=function(){
+      var matchReport=document.createElement("script");
+      matchReport.src="js/imc-match-report-beta.js?v=1.0.0";
+      document.head.appendChild(matchReport);
+    };
     document.head.appendChild(scorers);
   };
   document.head.appendChild(core);
