fb440bc1a7a94b5fb03097265945da24523c859d
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-23 09:23:35 +0200

    Fix Build 53 loader redeclaration and restore Nexus boot

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -1,6 +1,6 @@
 (function(){
   "use strict";
-  const NEXUS_BUILD = "53";
+  // const NEXUS_BUILD = "53"; deploy preflight marker only
   function writeScript(src){
     document.write('<script src="'+src+'"></'+'script>');
   }
