d4d41e7be3668574d6dc6ad3fe946bce0eebe298
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 06:42:33 +0200

    Nexus: load approved Competition card design

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -58,7 +58,7 @@
     appendScript("js/imc-player-codex-global.js?v=1.0.0");
     appendScript("js/imc-player-codex-global-detail.js?v=1.1.0");
     appendScript("js/imc-competitions.js?v=1.2.0",function(){
-      appendScript("js/imc-competition-ui.js?v=1.0.0");
+      appendScript("js/imc-competition-ui.js?v=1.1.0");
     });
   }
 
@@ -75,7 +75,7 @@
     writeScript("js/imc-player-codex-global.js?v=1.0.0");
     writeScript("js/imc-player-codex-global-detail.js?v=1.1.0");
     writeScript("js/imc-competitions.js?v=1.2.0");
-    writeScript("js/imc-competition-ui.js?v=1.0.0");
+    writeScript("js/imc-competition-ui.js?v=1.1.0");
     return;
   }
 
