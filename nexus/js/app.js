62443e59973b26176e5f9f4b687fc7b197524e95
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 12:36:44 +0200

    Load Starting XI layout for GW001 Match Report

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -60,10 +60,8 @@
     appendScript("js/imc-competitions.js?v=1.3.0",function(){
       appendScript("js/imc-competition-ui.js?v=1.2.1",function(){
         appendScript("js/imc-match-report-gw001.js?v=0.1.0",function(){
-          appendScript("js/imc-match-report-lineups-gw001.js?v=0.2.0",function(){
-            appendScript("js/imc-match-report-formation-map-gw001.js?v=0.1.0",function(){
-              appendScript("js/imc-match-report-ratings-gw001.js?v=0.1.1");
-            });
+          appendScript("js/imc-match-report-lineups-gw001.js?v=0.3.0",function(){
+            appendScript("js/imc-match-report-ratings-gw001.js?v=0.1.1");
           });
         });
       });
@@ -85,8 +83,7 @@
     writeScript("js/imc-competitions.js?v=1.3.0");
     writeScript("js/imc-competition-ui.js?v=1.2.1");
     writeScript("js/imc-match-report-gw001.js?v=0.1.0");
-    writeScript("js/imc-match-report-lineups-gw001.js?v=0.2.0");
-    writeScript("js/imc-match-report-formation-map-gw001.js?v=0.1.0");
+    writeScript("js/imc-match-report-lineups-gw001.js?v=0.3.0");
     writeScript("js/imc-match-report-ratings-gw001.js?v=0.1.1");
     return;
   }
