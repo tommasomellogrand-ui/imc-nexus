4c6edad0cb37f48a3f524a1d4ae69601e748390d
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 10:59:40 +0200

    Load GW001 formation-only Match Report lineups

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -59,7 +59,9 @@
     appendScript("js/imc-player-codex-global-detail.js?v=1.1.0");
     appendScript("js/imc-competitions.js?v=1.3.0",function(){
       appendScript("js/imc-competition-ui.js?v=1.2.1",function(){
-        appendScript("js/imc-match-report-gw001.js?v=0.1.0");
+        appendScript("js/imc-match-report-gw001.js?v=0.1.0",function(){
+          appendScript("js/imc-match-report-lineups-gw001.js?v=0.1.0");
+        });
       });
     });
   }
@@ -79,6 +81,7 @@
     writeScript("js/imc-competitions.js?v=1.3.0");
     writeScript("js/imc-competition-ui.js?v=1.2.1");
     writeScript("js/imc-match-report-gw001.js?v=0.1.0");
+    writeScript("js/imc-match-report-lineups-gw001.js?v=0.1.0");
     return;
   }
 
