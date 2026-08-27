2237ab122ae019981bb7332027b357d544b88c5f
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 11:37:59 +0200

    Load GW001 editorial lineups v0.2.0

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -60,7 +60,7 @@
     appendScript("js/imc-competitions.js?v=1.3.0",function(){
       appendScript("js/imc-competition-ui.js?v=1.2.1",function(){
         appendScript("js/imc-match-report-gw001.js?v=0.1.0",function(){
-          appendScript("js/imc-match-report-lineups-gw001.js?v=0.1.0");
+          appendScript("js/imc-match-report-lineups-gw001.js?v=0.2.0");
         });
       });
     });
@@ -81,7 +81,7 @@
     writeScript("js/imc-competitions.js?v=1.3.0");
     writeScript("js/imc-competition-ui.js?v=1.2.1");
     writeScript("js/imc-match-report-gw001.js?v=0.1.0");
-    writeScript("js/imc-match-report-lineups-gw001.js?v=0.1.0");
+    writeScript("js/imc-match-report-lineups-gw001.js?v=0.2.0");
     return;
   }
 
