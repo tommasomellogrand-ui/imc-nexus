28a5fe3c701bdafd70c81185913d68bc7e6eadf9
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 15:54:25 +0200

    Load dual-team text Starting XI v0.5.0

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -60,7 +60,7 @@
     appendScript("js/imc-competitions.js?v=1.3.0",function(){
       appendScript("js/imc-competition-ui.js?v=1.2.1",function(){
         appendScript("js/imc-match-report-gw001.js?v=0.1.0",function(){
-          appendScript("js/imc-match-report-lineups-gw001.js?v=0.4.0",function(){
+          appendScript("js/imc-match-report-lineups-gw001.js?v=0.5.0",function(){
             appendScript("js/imc-match-report-ratings-gw001.js?v=0.1.1");
           });
         });
@@ -83,7 +83,7 @@
     writeScript("js/imc-competitions.js?v=1.3.0");
     writeScript("js/imc-competition-ui.js?v=1.2.1");
     writeScript("js/imc-match-report-gw001.js?v=0.1.0");
-    writeScript("js/imc-match-report-lineups-gw001.js?v=0.4.0");
+    writeScript("js/imc-match-report-lineups-gw001.js?v=0.5.0");
     writeScript("js/imc-match-report-ratings-gw001.js?v=0.1.1");
     return;
   }
