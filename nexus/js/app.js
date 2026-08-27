662bfd8792ed531d4c09238a444c007feba117c6
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-27 08:51:11 +0200

    Nexus: load Competition ordering and trophy fix

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -57,8 +57,8 @@
     appendScript("js/imc-player-codex-menu.js?v=1.2.0");
     appendScript("js/imc-player-codex-global.js?v=1.0.0");
     appendScript("js/imc-player-codex-global-detail.js?v=1.1.0");
-    appendScript("js/imc-competitions.js?v=1.2.0",function(){
-      appendScript("js/imc-competition-ui.js?v=1.1.0");
+    appendScript("js/imc-competitions.js?v=1.2.1",function(){
+      appendScript("js/imc-competition-ui.js?v=1.2.0");
     });
   }
 
@@ -74,8 +74,8 @@
     writeScript("js/imc-player-codex-menu.js?v=1.2.0");
     writeScript("js/imc-player-codex-global.js?v=1.0.0");
     writeScript("js/imc-player-codex-global-detail.js?v=1.1.0");
-    writeScript("js/imc-competitions.js?v=1.2.0");
-    writeScript("js/imc-competition-ui.js?v=1.1.0");
+    writeScript("js/imc-competitions.js?v=1.2.1");
+    writeScript("js/imc-competition-ui.js?v=1.2.0");
     return;
   }
 
