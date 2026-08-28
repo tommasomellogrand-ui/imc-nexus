bcaf71bf5b95fd98bdac2369bb4bf5082ed6c3f6
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-28 11:34:13 +0200

    feat: publish GW007 Competition Stats v1.1.0

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -61,7 +61,7 @@
     appendScript("js/imc-nexus-client-bridge.js?v=1.0.0",function(){
       appendScript("js/imc-match-report-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competitions-all-worlds.js?v=1.0.0");
-      appendScript("js/imc-gw007-competition-stats.js?v=1.0.0");
+      appendScript("js/imc-gw007-competition-stats.js?v=1.1.0");
     });
   }
 
@@ -81,7 +81,7 @@
     writeScript("js/imc-nexus-client-bridge.js?v=1.0.0");
     writeScript("js/imc-match-report-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competitions-all-worlds.js?v=1.0.0");
-    writeScript("js/imc-gw007-competition-stats.js?v=1.0.0");
+    writeScript("js/imc-gw007-competition-stats.js?v=1.1.0");
     return;
   }
 
