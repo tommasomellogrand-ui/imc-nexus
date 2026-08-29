0390b2309b973148b97aefb07f90136909bcadb3
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-29 10:32:38 +0200

    Bump GW008 entities cache version

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -33,7 +33,7 @@
 
   var clubHouseTimer=null;
   new MutationObserver(function(){
-    clearTimeout(clubHouseTimer);
+    clearTimeout(clHouseTimer);
     clubHouseTimer=setTimeout(removeClubHouseManagerHero,80);
   }).observe(document.documentElement,{childList:true,subtree:true});
   removeClubHouseManagerHero();
@@ -67,7 +67,7 @@
       appendScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
       appendScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
       appendScript("js/imc-managers-gw008.js?v=1.0.1");
-      appendScript("js/imc-entities-gw008.js?v=1.0.1");
+      appendScript("js/imc-entities-gw008.js?v=1.0.2");
     });
   }
 
@@ -93,7 +93,7 @@
     writeScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
     writeScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
     writeScript("js/imc-managers-gw008.js?v=1.0.1");
-    writeScript("js/imc-entities-gw008.js?v=1.0.1");
+    writeScript("js/imc-entities-gw008.js?v=1.0.2");
     return;
   }
 
