393c978a5911051718a751d56d47f37730e2f11a
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-29 16:31:13 +0200

    Load standalone Trophy Room module

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -82,6 +82,7 @@
       appendScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
       appendScript("js/imc-managers-gw008.js?v=1.5.0");
       appendScript("js/imc-team-hub-gw008.js?v=2.2.0");
+      appendScript("js/imc-trophy-room-all-worlds.js?v=1.0.0");
     });
   }
 
@@ -108,6 +109,7 @@
     writeScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
     writeScript("js/imc-managers-gw008.js?v=1.5.0");
     writeScript("js/imc-team-hub-gw008.js?v=2.2.0");
+    writeScript("js/imc-trophy-room-all-worlds.js?v=1.0.0");
     setTimeout(syncVisibleBuildLabel,0);
     return;
   }
