1b0d550ce6a1382988726c9bfd8c2f8ae6362135
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-29 17:33:06 +0200

    Use integrated Competition Trophy Room runtime

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -74,8 +74,7 @@
     appendScript("js/imc-gw001-transfer-inline.js?v=1.0.0");
     appendScript("js/imc-nexus-client-bridge.js?v=1.0.0",function(){
       appendScript("js/imc-match-report-all-worlds.js?v=1.0.0");
-      appendScript("js/imc-competitions-all-worlds.js?v=1.0.0");
-      appendScript("js/imc-competition-trophy-room-gw008.js?v=1.0.0");
+      appendScript("js/imc-competitions-all-worlds.js?v=1.1.0");
       appendScript("js/imc-competition-iphone-typography-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
@@ -100,8 +99,7 @@
     writeScript("js/imc-gw001-transfer-inline.js?v=1.0.0");
     writeScript("js/imc-nexus-client-bridge.js?v=1.0.0");
     writeScript("js/imc-match-report-all-worlds.js?v=1.0.0");
-    writeScript("js/imc-competitions-all-worlds.js?v=1.0.0");
-    writeScript("js/imc-competition-trophy-room-gw008.js?v=1.0.0");
+    writeScript("js/imc-competitions-all-worlds.js?v=1.1.0");
     writeScript("js/imc-competition-iphone-typography-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
