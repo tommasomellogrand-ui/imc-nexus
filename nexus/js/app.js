a7392c2aaaced7a7954705e7b8fe1f1158db71a9
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-29 09:54:33 +0200

    Load competition filters v2.2

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -64,7 +64,7 @@
       appendScript("js/imc-competition-stats-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competition-iphone-typography-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
-      appendScript("js/imc-competition-country-type-filters.js?v=2.1.0");
+      appendScript("js/imc-competition-country-type-filters.js?v=2.2.0");
       appendScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
       appendScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
       appendScript("js/imc-managers-gw008.js?v=1.0.1");
@@ -90,7 +90,7 @@
     writeScript("js/imc-competition-stats-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competition-iphone-typography-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
-    writeScript("js/imc-competition-country-type-filters.js?v=2.1.0");
+    writeScript("js/imc-competition-country-type-filters.js?v=2.2.0");
     writeScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
     writeScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
     writeScript("js/imc-managers-gw008.js?v=1.0.1");
