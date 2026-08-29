7d123c71a74810af2b9da0e2603796dfea36ad52
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-08-29 11:00:05 +0200

    Load GW008 managers v1.0.2

--- nexus/js/app.js
+++ nexus/js/app.js
@@ -66,7 +66,7 @@
       appendScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
       appendScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
       appendScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
-      appendScript("js/imc-managers-gw008.js?v=1.0.1");
+      appendScript("js/imc-managers-gw008.js?v=1.0.2");
       appendScript("js/imc-entities-gw008.js?v=1.0.2");
     });
   }
@@ -92,7 +92,7 @@
     writeScript("js/imc-competition-premium-list-all-worlds.js?v=1.0.0");
     writeScript("js/imc-competition-card-insights-all-worlds.js?v=1.0.0");
     writeScript("js/imc-gw001-competition-manager-labels.js?v=1.1.0");
-    writeScript("js/imc-managers-gw008.js?v=1.0.1");
+    writeScript("js/imc-managers-gw008.js?v=1.0.2");
     writeScript("js/imc-entities-gw008.js?v=1.0.2");
     return;
   }
