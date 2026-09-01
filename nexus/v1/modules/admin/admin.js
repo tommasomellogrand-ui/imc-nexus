6588c2d0d82dc22865d30c487c021bc0b5ead3f0
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-01 21:57:23 +0200

    Remove uncreated GW010 from Admin Manager Registry

--- nexus/v1/modules/admin/admin.js
+++ nexus/v1/modules/admin/admin.js
@@ -2,10 +2,10 @@
 "use strict";
 if(window.IMC_ADMIN)return;
 
-const VERSION="1.0.0";
+const VERSION="1.0.1";
 const WORLDS=[
   ["GW001","Road To History"],["GW002","Gold 558"],["GW003","Gold 557"],["GW004","World League"],["GW005","Hall Of Famers"],
-  ["GW006","Master League World"],["GW007","The Four Kingdoms"],["GW008","Gold 1"],["GW009","Kick Off"],["GW010","Sensible Soccer Academy"]
+  ["GW006","Master League World"],["GW007","The Four Kingdoms"],["GW008","Gold 1"],["GW009","Kick Off"]
 ];
 let state={container:null,client:null,user:null,page:"home",worldId:"",data:null,newEditor:null};
 
